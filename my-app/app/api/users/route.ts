94be6ef6047e5a812017ba43fc3e061a00a67f08
Ally <alianeamaral.tec@gmail.com>
2025-08-02 09:56:22 -0400

    insere try catch do banco

--- my-app/app/api/users/route.ts
+++ my-app/app/api/users/route.ts
@@ -2,15 +2,23 @@ import db from '@/app/api/database';
 
 export async function GET(request: Request){
 
-    const [rows] = await db.query("SELECT * FROM users")
+    try {
+        const [rows] = await db.query("SELECT * FROM users")
+        return new Response(JSON.stringify(rows), {
+            status: 200,
+            headers: {'Content-Type': 'application/json'}
+        })
+    
+    }
+
+    catch (error) {
+        return new Response(JSON.stringify({erro: error}),
+            {status: 500}
+        )
+    }
 
     // const users = [
     //     {id:1, name:"Rafael"},
     //     {id:2, name:"Vinicius"}
     // ]
-
-    return new Response(JSON.stringify(rows), {
-        status: 200,
-        headers: {'Content-Type': 'application/json'}
-    })
 }
